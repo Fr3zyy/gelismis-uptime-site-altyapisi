60b4158374ed3d10d33bec3a30dae44eacd8e6f9
Fr3zyy <123799898+Fr3zyy@users.noreply.github.com>
2023-04-25 22:50:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,19 +9,5 @@ client.once('ready', () => {
   console.log(`Ready! Logged in as ${client.user.tag}`);
 });
 
-client.on('messageCreate', async (message) => {
-  if (message.channel.id === '1086408253413523537') {
-    if (message.content.includes('http')) {
-      const embed = new MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle('Link eklendi:')
-        .setDescription(message.content);
-      
-      const channel = await client.channels.fetch('1086408253413523537');
-      channel.send({ embeds: [embed] });
-    }
-  }
-});
-
 client.login(configs.token);
 module.exports = { client };
