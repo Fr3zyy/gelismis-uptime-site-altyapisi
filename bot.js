387adc5c99a8d03b0238fca11fc16b457280d2a2
Fr3zyy <123799898+Fr3zyy@users.noreply.github.com>
2023-04-25 22:49:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,17 +10,13 @@ client.once('ready', () => {
 });
 
 client.on('messageCreate', async (message) => {
-  // Check if message is posted in the specified channel
   if (message.channel.id === '1086408253413523537') {
-    // Check if message contains a link
     if (message.content.includes('http')) {
-      // Create a new embed message with the link
       const embed = new MessageEmbed()
         .setColor('#0099ff')
         .setTitle('Link eklendi:')
         .setDescription(message.content);
       
-      // Send the embed message to the channel
       const channel = await client.channels.fetch('1086408253413523537');
       channel.send({ embeds: [embed] });
     }
